fa9d5110ce689c46ffe04f79b14c8aa907510b34
Anushka Pawar <apawar@usf.edu>
2019-09-04 12:40:36 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace IAM4300MYFIRSTAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is my first console app");
+            Console.WriteLine("I am learning how to publish to GitHub");
+            Console.ReadKey();
+        }
+    }
+}
